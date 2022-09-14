d6be0f9030e795e04dc8d2c708aca060d4065371
franklinselva <franklinselva10@gmail.com>
2022-09-14 14:38:44 +0200

    fix CT_drone component

--- start.tcl
+++ start.tcl
@@ -1,13 +1,13 @@
 package require genomix
 genomix::connect
-genomix1 rpath /home/felix/work/lib/genom/ros/plugins/
+genomix1 rpath $::env(DRONE_VV_PATH)/lib/genom/ros/plugins/
 genomix1 load CT_robot
 
 proc init {} {
     CT_robot::SetVerbose 1
-    CT_robot::Set_my_r 3 
+    CT_robot::Set_my_r 3
     CT_robot::Set_my_g 2
     CT_robot::Set_my_b 105
     CT_robot::Set_my_seuil 40
     CT_robot::connect_port {local ImagePort remote /camera/image_raw}
-} 
+}
